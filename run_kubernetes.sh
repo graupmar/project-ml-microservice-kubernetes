a453354f3a50822da0b389378a29b122c92f0a19
graupmar <marco.graupmann@tui.com>
2021-07-27 14:02:42 +0000

    added changes until task 8 of project

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="graupma/udacityapp:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacityapp \
+        --image=$dockerpath \
+        --port=80
 
 # Step 3:
 # List kubernetes pods
+sleep 10
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacityapp 8000:80
